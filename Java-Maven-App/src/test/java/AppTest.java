8f543b9a14415ce43d4a9990f5a62353e05704b9
dp1008 <38103879+dp1008@users.noreply.github.com>
2022-08-02 12:45:14 +0530

    Add files via upload

---
+++ Java-Maven-App/src/test/java/AppTest.java
@@ -0,0 +1,12 @@
+import com.example.Application;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+	@Test
+	public void testApp() {
+		Application myApp = new Application();
+		string result = myApp.getStatus();
+		assertEquals("OK", result);
+	}
+}
